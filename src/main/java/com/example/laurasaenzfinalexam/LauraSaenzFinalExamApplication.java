e7646c28f6b62c71f5941b3186c526e7931b15bf
Laura <ximesaenz@gmail.com>
2023-08-11 16:06:17 -0700

    Initial commit

---
+++ src/main/java/com/example/laurasaenzfinalexam/LauraSaenzFinalExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.laurasaenzfinalexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LauraSaenzFinalExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LauraSaenzFinalExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/laurasaenzfinalexam/LauraSaenzFinalExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.laurasaenzfinalexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LauraSaenzFinalExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
